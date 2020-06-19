5be438d8443e13525d72a758ec58489bc3a7d745
KirillYatsenko <kiriyatsenko@gmail.com>
2020-06-19 10:01:05 +0300

    5.9 -  change indexes to pointers

--- exercises/chapter-5/5.9/main.c
+++ exercises/chapter-5/5.9/main.c
@@ -6,7 +6,7 @@ void month_day(int year, int yearday, int *pmonth, int *pday);
 
 int main()
 {
-    int day = day_of_year(0, 05, 31);
+    int day = day_of_year(1997, 05, 31);
     printf("day = %d\n", day);
 
     int month;
@@ -40,14 +40,15 @@ int day_of_year(int year, int month, int day)
     int i, leap;
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 
-    if(day <= 1 || day > daytab[leap][month])
+    if(day <= 1 || day > *(*(daytab + leap) + month))
     {
         printf("\n ERROR: 'day' param is not correct");
         return -1;
     }
 
     for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+        day += *(*(daytab + leap) + i);
+
     return day;
 }
 
@@ -69,8 +70,9 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
         return;
     }
 
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
+    for (i = 1; yearday > *(*(daytab + leap) + i); i++)
+        yearday -= *(*(daytab + leap) + i);
+
     *pmonth = i;
     *pday = yearday;
 }
