eb2b8c2a4c9fa9294d1a6e0077c665766520eba4
KirillYatsenko <kiriyatsenko@gmail.com>
2020-06-24 21:43:43 +0300

    5.15 - add option for ignoring case

--- exercises/chapter-5/5.14/main.c
+++ exercises/chapter-5/5.14/main.c
@@ -9,11 +9,13 @@ char *lineptr[MAXLINES];
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-void qsort(void *v[], int left, int right, int (*comp)(void *, void *), int (*ord)(int));
+void qsort(void *v[], int left, int right, int (*comp)(void *, void *), int (*ord)(int), char* (*consider_case)(char*));
 int numcmp(char *, char *);
 int _getline(char line[], int maxLine);
 int asc(int a);
 int desc(int a);
+char* ingore_case(char* a);
+char* case_sensetive(char* a);
 
 
 main(int argc, char *argv[])
@@ -21,6 +23,7 @@ main(int argc, char *argv[])
     int nlines;
     int numeric = 0;
     int reverse = 0;
+    int ignorecase = 0;
 
     for(int i = 1; i < argc; ++i)
     {
@@ -28,12 +31,14 @@ main(int argc, char *argv[])
             numeric = 1;
         else if(strcmp(argv[i], "-r") == 0)
             reverse = 1;
+        else if(strcmp(argv[i], "-f") == 0)
+            ignorecase = 1;
     }
 
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
     {
         qsort((void**) lineptr, 0, nlines-1,
-              (int (*)(void*,void*))(numeric ? numcmp : strcmp), (int (*)(int))(reverse ? desc : asc));
+              (int (*)(void*,void*))(numeric ? numcmp : strcmp), (int (*)(int))(reverse ? desc : asc), (char (*)(char))(ignorecase? ingore_case : case_sensetive) );
         writelines(lineptr, nlines);
         return 0;
     }
@@ -54,8 +59,24 @@ int asc(int a)
     return a < 0;
 }
 
+char* ingore_case(char* a)
+{
+   char *a_copy = malloc(strlen(a));
+   strcpy(a_copy, a);
+
+    for(int i = 0; *(a_copy + i); i++)
+        *(a_copy + i) = tolower(*(a_copy + i));
+
+   return a_copy;
+}
+
+char* case_sensetive(char* a)
+{
+    return a;
+}
+
 /* qsort: sort v[left]...v[right] into increasing order */
-void qsort(void *v[], int left, int right, int (*comp)(void *, void *), int (*ord)(int))
+void qsort(void *v[], int left, int right, int (*comp)(void *, void *), int (*ord)(int), char* (*consider_case)(char*))
 {
     int i, last;
     void swap(void *v[], int, int);
@@ -65,14 +86,18 @@ void qsort(void *v[], int left, int right, int (*comp)(void *, void *), int (*or
     swap(v, left, (left + right)/2);
     last = left;
 
-    for (i = left+1; i <= right; i++)
-        //if ((*comp)(v[i], v[left]) > 0)
-        if ((*ord)((*comp)(v[i], v[left])))
+    for (i = left+1; i <= right; i++){
+
+        char* a = consider_case(v[i]);
+        char* b = consider_case(v[left]);
+
+        if ((*ord)((*comp)(a, b)))
             swap(v, ++last, i);
+    }
 
     swap(v, left, last);
-    qsort(v, left, last-1, comp, ord);
-    qsort(v, last+1, right, comp, ord);
+    qsort(v, left, last-1, comp, ord, consider_case);
+    qsort(v, last+1, right, comp, ord, consider_case);
 }
 
 int numcmp(char *s1, char *s2)
