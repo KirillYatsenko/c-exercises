ee70d8bed59f46b4dbc5c758fbbfa5c47a47806a
KirillYatsenko <kiriyatsenko@gmail.com>
2020-07-06 10:31:29 +0300

    6.3 - solved

--- exercises/chapter-6/6.3/main.c
+++ exercises/chapter-6/6.3/main.c
@@ -6,36 +6,59 @@
 #define MAXWORD 100
 #define BUFSIZE 100
 
+char *ignore[] =
+    {
+        "a",
+        "an",
+        "this",
+        "the",
+        "is"};
+
 struct tnode
 {
     char *word;
-    int count;
+    int line;
     struct tnode *left;
     struct tnode *right;
 };
 
-struct tnode *addtree(struct tnode *, char *);
+struct tnode *addtree(struct tnode *, char *, int line);
 void treeprint(struct tnode *);
-int getword(char *, int);
+int getword(char *, int, int *);
 struct tnode *talloc(void);
 char *_strdup(char *);
+int needignore(char *w);
 
 char buf[BUFSIZE];
 int bufp = 0;
 
 int main()
 {
+    int line = 1;
+    int linec = 1;
     struct tnode *root;
     char word[MAXWORD];
     root = NULL;
-    while (getword(word, MAXWORD) != EOF)
-        if (isalpha(word[0]))
-            root = addtree(root, word);
+    while (getword(word, MAXWORD, &linec) != EOF)
+        if (isalpha(word[0]) && !needignore(word))
+        {
+            root = addtree(root, word, line);
+            line = linec;
+        }
     treeprint(root);
     return 0;
 }
 
-struct tnode *addtree(struct tnode *p, char *w)
+int needignore(char *w)
+{
+    for (int i = 0; *(ignore + i); i++)
+        if (strcmp(w, *(ignore + i)) == 0)
+            return 1;
+
+    return 0;
+}
+
+struct tnode *addtree(struct tnode *p, char *w, int line)
 {
     int cond;
     if (p == NULL)
@@ -44,18 +67,15 @@ struct tnode *addtree(struct tnode *p, char *w)
         p = talloc();
         /* make a new node */
         p->word = _strdup(w);
-        p->count = 1;
+        p->line = line;
         p->left = p->right = NULL;
     }
-    else if ((cond = strcmp(w, p->word)) == 0)
-        p->count++;
-    /* repeated word */
-    else if (cond < 0)
+    else if ((cond = strcmp(w, p->word)) < 0)
         /* less than into left subtree */
-        p->left = addtree(p->left, w);
-    else
+        p->left = addtree(p->left, w, line);
+    else if (cond > 0)
         /* greater than into right subtree */
-        p->right = addtree(p->right, w);
+        p->right = addtree(p->right, w, line);
     return p;
 }
 
@@ -65,7 +85,7 @@ void treeprint(struct tnode *p)
     if (p != NULL)
     {
         treeprint(p->left);
-        printf("%4d %s\n", p->count, p->word);
+        printf("line number - %d %s\n", p->line, p->word);
         treeprint(p->right);
     }
 }
@@ -86,7 +106,7 @@ char *_strdup(char *s)
     return p;
 }
 
-int getword(char *word, int lim)
+int getword(char *word, int lim, int *line)
 {
     int c, getch(void);
     void ungetch(int);
@@ -103,6 +123,9 @@ int getword(char *word, int lim)
     for (; --lim > 0; w++)
         if (!isalnum(*w = getch()))
         {
+            if (*w == '\n')
+                (*line)++;
+
             ungetch(*w);
             break;
         }
@@ -121,4 +144,4 @@ void ungetch(int c)
         printf("ungetch: too many characters\n");
     else
         buf[bufp++] = c;
-} 
+}
