c378d6840db50dde97c34983bbb2dc7e42a633f1
KirillYatsenko <kiriyatsenko@gmail.com>
2020-07-14 17:32:15 +0300

    8.1 - rewrite reading/writing to file by using syscalls

--- exercises/chapter-8/8.1/main.c
+++ exercises/chapter-8/8.1/main.c
@@ -1,31 +1,39 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#define BUFSIZE 200
+int buf[BUFSIZE];
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int i = 0;
+
+    int fd;
+    void filecopy(int fd, char *, int);
 
     if (argc == 1)
-        filecopy(stdin, stdout);
+        filecopy(0, "stdin", 1);
     else
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL)
+            if ((fd = open(*++argv, O_RDONLY, 0)) == -1)
             {
                 printf("cat: can't open %s\n", *argv);
                 return 1;
             }
             else
             {
-                filecopy(fp, stdout);
-                fclose(fp);
+                filecopy(fd, *argv, 1);
+                close(fd);
             }
     return 0;
 }
 
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int fd, char *filename, int fo)
 {
-    int c;
+    int n;
 
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
+    while ((n = read(fd, buf, BUFSIZE)) > 0)
+        if (write(fo, buf, n) != n)
+            printf("\nerror: while reading '%s' file", filename);
 }
